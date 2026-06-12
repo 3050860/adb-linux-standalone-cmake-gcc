30cc771cf61c29c55a51e39d49bce3984600d9df
Roman N <3050860@gmail.com>
2026-06-12 16:54:31 +0300

    shrink code

--- adb/fdevent/fdevent.cpp
+++ adb/fdevent/fdevent.cpp
@@ -1,19 +1,3 @@
-/*
- * Copyright 2006, Brian Swetland <swetland@frotz.net>
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
 #define TRACE_TAG FDEVENT
 
 #include "sysdeps.h"
@@ -27,10 +11,6 @@
 #include "adb_utils.h"
 #include "fdevent.h"
 #include "fdevent_epoll.h"
-
-#if !defined(__linux__)
-#include "fdevent_poll.h"
-#endif
 
 using namespace std::chrono_literals;
 using std::chrono::duration_cast;
@@ -201,11 +181,7 @@ void fdevent_context::TerminateLoop() {
 }
 
 static std::unique_ptr<fdevent_context> fdevent_create_context() {
-#if defined(__linux__)
     return std::make_unique<fdevent_context_epoll>();
-#else
-    return std::make_unique<fdevent_context_poll>();
-#endif
 }
 
 static auto& g_ambient_fdevent_context() {
